3860b803921d1e7291ac35593518291b9fe13c37
Jose Eduardo Silva Dias <J.eduardodias@outlook.com>
2025-07-06 19:05:20 -0300

    selecionar o tipo de cômodo
    Introduz solicitações ao usuário para selecionar o tipo de cômodo e lê a escolha do usuário. Isso prepara o programa para uma lógica adicional com base no tipo de cômodo selecionado.

--- AtividadePratica4/Pratica4.cpp
+++ AtividadePratica4/Pratica4.cpp
@@ -1,10 +1,18 @@
 
 #include <stdio.h>
 #include <locale.h>
-#include <math.h> // pra usar o ceil()
+#include <math.h>
 
 int main() {
-	setlocale(LC_ALL, "Portuguese"); // para aceitar os acentos
+	setlocale (LC_ALL,  "Portuguese"); // para  aceitar os acentos
 
 	int tipoComodo;
 	float comp, larg, area, iluminancia, fluxoTotal, qtdLamp;
+
+	// mostra as  opções
+	printf("Selecione o tipo de comodo:\n");
+	printf("1 - Quarto\n");
+	printf("2 - Escritotio \n");
+	printf("3 - Cozinha\n ");
+	printf("Digite a opção desejada: ");
+	scanf_s("%d", &tipoComodo );
