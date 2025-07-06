e8b2f6508785ceaa50ce71768f2544ef5ed8d746
Jose Eduardo Silva Dias <J.eduardodias@outlook.com>
2025-07-06 19:21:34 -0300

    correçoes

--- AtividadePratica4/Pratica4.cpp
+++ AtividadePratica4/Pratica4.cpp
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <locale.h>
-#include <math.h>
+#include <math.h> 
 
 int main() {
-    setlocale(LC_ALL, "Portuguese"); // para aceitar acentos
+    setlocale(LC_ALL, "Portuguese"); // pra aceitar acentos
 
     int tipo;
     float comp, larg, area, iluminancia, fluxoTotal, qtdLamp;
 
     
     printf("Selecione o tipo de cômodo:\n1 - Quarto\n2 - Escritório\n3 - Cozinha\nDigite a opção desejada: ");
-    scanf_s("%d", &tipo);//solocita o tipo do comomo
+	scanf_s("%d", &tipo); // lê tipo de comomo
 
     printf("Digite o comprimento do cômodo (m): ");
-	scanf_s("%f", &comp);// solicita o comprimento do cômodo
+	scanf_s("%f", &comp); // lê comprimento
+
     printf("Digite a largura do cômodo (m): ");
-	scanf_s("%f", &larg);// solicita a largura do cômodo
+	scanf_s("%f", &larg); // lê largura
 
-    // calcula a área
-    area = comp * larg;
 
-    // define iluminância conforme tipo
+	area = comp * larg; // calcula área
+
+    // define iluminacia conforme tipo
     if (tipo == 1)
         iluminancia = 150;
     else
         iluminancia = 300;
 
-    // calcula fluxo total necessário
+    // calcula  fluxo total necessario
     fluxoTotal = area * iluminancia / 0.8;
 
     // calcula qtd mínima de lâmpadas
     qtdLamp = ceil(fluxoTotal / 800);
 
-    // saída
-    printf("\nTipo de cômodo: %d (Iluminância recomendada: %.0f lux)\n", tipo, iluminancia);
-    printf("Área do cômodo: %.2f m²\n", area);
-    printf("Fluxo luminoso total necessário: %.2f lúmens\n", fluxoTotal);
-    printf("Quantidade mínima de lâmpadas recomendadas: %.0f\n", qtdLamp);
+    
+	printf("\nTipo de cômodo: "); // exibe tipo de cômodo
+    if (tipo == 1)
+        printf("Quarto");
+    else if (tipo == 2)
+        printf("Escritprio");
+    else
+        printf("Cozinha");
+
+	printf(" (Iluminâacia recomendada: %.0f lux)\n", iluminancia);  // mostra iluminancia recomendada
+	printf("Area do comodo: %.2f m²\n", area); // mostra área do cômodo
+	printf("Fluxo luminoso total necessario: %.2f lúmens\n", fluxoTotal); // mostra fluxo luminoso total necessário
+	printf("Quantidade minima de lampadas recomndadas: %.0f\n", qtdLamp); // mostra quantidade mínima de lâmpadas recomendadas
 
     return 0;
 }
